135aa5c029f73659378dd2e667318d1f058a05a6
michel <michelbiron1@gmail.com>
2011-11-07 16:28:59 -0500

    Modification de la fiche médicale

--- src/app/controllers/inscrire_enfant_controller.php
+++ src/app/controllers/inscrire_enfant_controller.php
@@ -28,11 +28,12 @@ class InscrireEnfantController extends AppController {
 		$this->set('title_for_layout', __('Inscription d\'un enfant', true));
 		$this->set('titre',__('Fiche médicale',true));
 		$this->set('ariane', __('Informations générales > <span style="color: green;">Fiches médicales</span> > Autorisations', true));
+		$this->loadModel("Medicament");
 		pr($this->getMaladieList());
 	}
 	
 	public function getMaladieList(){
-		//return $this->Medicament->find('all');
+		return $this->Medicament->find('all');
 	}
 }
 ?>
