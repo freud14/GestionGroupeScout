856c93b24369242c6421aeb256241cf9dfd6ae13
Frédérik Paradis <fredy_14@live.fr>
2011-11-07 14:27:10 -0500

    Mise à jour du contrôleur, du modèle et de la vue de InscrireEnfant

--- src/app/controllers/inscrire_enfant_controller.php
+++ src/app/controllers/inscrire_enfant_controller.php
@@ -8,9 +8,19 @@ class InscrireEnfantController extends AppController {
 	}
 	
 	function index() {
-		$this->set('title_for_layout', __('Inscription d\'un enfant', true));
-		$this->set('titre', __('Informations générales', true));
-		$this->set('ariane', __('<span style="color: green;">Informations générales</span> > Fiches médicales > Autorisations', true));
+		if (!empty($this->data)) {
+			$this->InscrireEnfant->set($this->data);
+			if($this->InscrireEnfant->validates()) {
+				echo "valide";
+			} else {
+				echo "invalide";
+			}		
+		}
+		//else {
+			$this->set('title_for_layout', __('Inscription d\'un enfant', true));
+			$this->set('titre', __('Informations générales', true));
+			$this->set('ariane', __('<span style="color: green;">Informations générales</span> > Fiches médicales > Autorisations', true));
+		//}
 	}
 	
 	function fiche_medicale() {
--- src/app/models/inscrire_enfant.php
+++ src/app/models/inscrire_enfant.php
@@ -2,5 +2,16 @@
 class InscrireEnfant extends AppModel {
 	var $name = 'InscrireEnfant';
 	var $useTable = false;
+	
+	var $validate = array(
+		'nom' => array(
+			'rule' => array('minLength', 3), // ou bien : array('nomRegle', 'parametre1', 'parametre2' ...)
+			'required' => true,
+			'allowEmpty' => false,
+			'on' => 'create', // ou bien : 'update'
+			'message' => 'Votre message d\'erreur'
+		)
+	);
 }
+
 ?>
