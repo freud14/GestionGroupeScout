a17adba36ec1c5a302136b72ce795ad2d99868a7
Frédérik Paradis <fredy_14@live.fr>
2011-11-07 16:33:00 -0500

    InscrireEnfant...

--- src/app/controllers/inscrire_enfant_controller.php
+++ src/app/controllers/inscrire_enfant_controller.php
@@ -6,10 +6,10 @@ class InscrireEnfantController extends AppController {
 	function beforeFilter() {
 		parent::beforeFilter();
 		$this->layout = 'parent';
+		$this->loadModel('Malady');
 	}
 	
 	function index() {
-		$this->loadModel('Medicament');
 		if (!empty($this->data)) {
 			$this->InscrireEnfant->set($this->data);
 			if($this->InscrireEnfant->validates()) {
@@ -24,18 +24,17 @@ class InscrireEnfantController extends AppController {
 			$this->set('titre', __('Informations générales', true));
 			$this->set('ariane', __('<span style="color: green;">Informations générales</span> > Fiches médicales > Autorisations', true));
 		//}
-		pr($this->Medicament->find('all'));
+		//pr($this->getMaladieList());
 	}
 	
 	function fiche_medicale() {
 		$this->set('title_for_layout', __('Inscription d\'un enfant', true));
 		$this->set('titre',__('Fiche médicale',true));
 		$this->set('ariane', __('Informations générales > <span style="color: green;">Fiches médicales</span> > Autorisations', true));
-		pr($this->getMaladieList());
 	}
 	
 	public function getMaladieList(){
-		//return $this->Medicament->find('all');
+		return $this->Malady->find('all');
 	}
 }
 ?>
--- src/app/models/inscrire_enfant.php
+++ src/app/models/inscrire_enfant.php
@@ -19,7 +19,6 @@ class InscrireEnfant extends AppModel {
 						)
 			)
 	);
-
 }
 
 ?>
