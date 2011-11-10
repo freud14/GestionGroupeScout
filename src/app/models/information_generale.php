72123b29783528adf198725a0c9e22e1ac816521
Frédérik Paradis <fredy_14@live.fr>
2011-11-10 16:41:54 -0500

    informations générales

---
+++ src/app/models/information_generale.php
@@ -0,0 +1,23 @@
+<?php
+class InformationGenerale extends AppModel {
+	var $name = 'InformationGenerale';
+	var $useTable = false;
+	
+	var $validate = array(
+		'nom' => array(
+				'regle1' => array(
+						'rule' => array('maxLength', 45), //array('required', true),
+						'required' => true,
+						'allowEmpty' => false,
+						'on' => 'create',
+						'message' => 'Le champ ne peut être vide.'
+						),
+				'regle2' => array(
+						'rule' => array('maxLength', 45),
+						'message' => 'Le champ doit contenir moins de 45 caractères.'
+						)
+			)
+	);
+}
+
+?>
