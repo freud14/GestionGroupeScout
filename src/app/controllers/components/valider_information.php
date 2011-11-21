eee632272026c99e2e7418c79d20685b352398f3
michel <michelbiron1@gmail.com>
2011-11-21 13:26:43 -0500

    pas didee pour un titre

---
+++ src/app/controllers/components/valider_information.php
@@ -0,0 +1,24 @@
+<?php
+class ValiderInformationComponent extends Object {
+   
+   
+    function validerInformation($nom_utilisateur,$mot_de_passe)
+	{
+		
+		$resultat = null;
+    		$conditions = array("Compte.nom_utilisateur" => $nom_utilisateur,'Compte.mot_de_passe' => $mot_de_passe);    			
+    		$resultat = ClassRegistry::init('Compte')->find('first', array('conditions' => $conditions,'fields' => 'Compte.id'));
+    		
+		
+		if(!empty($resultat))
+		{
+			$resultat = array('autorisation' => $resultat['Autorisation'],'id_compte' => $resultat['Compte']['id'],'id_adulte' => $resultat['Adulte']['0']['id']);	
+		}
+			
+			
+		return $resultat;
+			//return $this->QuestionGenerale->find('all');
+	}
+    }
+    ?>
+
