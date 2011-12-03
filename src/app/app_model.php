e8d11d40dd535bd838c57b9800f0ccef6878afaf
Frederik <fredy_14@live.fr>
2011-12-02 22:33:22 -0500

    correction

--- src/app/app_model.php
+++ src/app/app_model.php
@@ -6,12 +6,12 @@ class AppModel extends Model {
 
     function afterFind($res, $bool) {
         parent::afterFind($res, $bool);
-        if (!$bool) {
+        /*if (!$bool) {
             return h($res, ENT_NOQUOTES);
         } else {
             return $res;
-        }
-        //return $res;
+        }*/
+        return $res;
         //return Sanitize::clean($res, array('encode' => true));
     }
 
